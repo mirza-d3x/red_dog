d62098806ac89e4678675d76df23bfb69f8680fd
Ashna <ashnakanakaraj704@gmail.com>
2024-03-21 10:19:31 +0400

    adding colors

---
+++ lib/styles/colors.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:flutter/material.dart';
+
+const loginBgColor = Color.fromRGBO(34, 43, 55,1);
+
+const redColor = Color.fromRGBO(255, 0, 0,1);
+
+const whiteColor = Color.fromRGBO(255, 255, 255,1);
+
+const unselectedRadioColor = Color.fromRGBO(217, 217, 217,1);
