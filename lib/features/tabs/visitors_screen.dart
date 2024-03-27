605758595bc88e0590a061a432eef60fc76ab8cd
Ashna <ashnakanakaraj704@gmail.com>
2024-03-27 11:09:39 +0400

    editing visitors screen

--- lib/features/tabs/visitors_screen.dart
+++ lib/features/tabs/visitors_screen.dart
@@ -126,94 +126,102 @@ class _VisitorsScreenState extends State<VisitorsScreen> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                        padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
-                        decoration: BoxDecoration(
-                          color: whiteColor,
-                          borderRadius: BorderRadius.circular(5),
-                          border: Border.all(color: titleTextColor)
-                        ),
-                        child: DropdownButtonHideUnderline(
-                          child: DropdownButton(
-                            icon: const Icon(
-                                Icons.keyboard_arrow_down_outlined,
-                              color: blackColor,
-                            ),
-                            // iconSize: 0,
-                            hint: selectedWebsite == null
-                                ? Row(
-                                  children: [
-                                    Text(
-                                    'Aladdinpro - GA4',
-                                    style: dropDownTextStyle
-                                    ),
+                      Card(
+                        elevation: 2,
+                        child: Container(
+                          padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                          decoration: BoxDecoration(
+                            color: whiteColor,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: DropdownButtonHideUnderline(
+                            child: DropdownButton(
+                              icon: const Icon(
+                                  Icons.keyboard_arrow_down_outlined,
+                                color: blackColor,
+                              ),
+                              // iconSize: 0,
+                              hint: selectedWebsite == null
+                                  ? Row(
+                                    children: [
+                                      Text(
+                                      'Aladdinpro - GA4',
+                                      style: dropDownTextStyle
+                                      ),
 
-                                    const SizedBox(width: 10),
-                                  ],
-                                )
-                                : Row(
-                                  children: [
-                                    Text(
-                                        selectedWebsite,
-                                    style: dropDownTextStyle
-                                    ),
-                                    const SizedBox(width: 10),
-                                  ],
-                                ),
-                            value: selectedWebsite,
-                            onChanged: (newValue) {
-                              setState(() {
-                                isSelectedFromDropDwn = true;
-                                selectedWebsite = newValue;
-                              });
-                            },
-                            items: [
-                              'Codelattice',
-                              'Alddinpro - GA4',
-                            ].map((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value,
-                                    style: dropDownTextStyle
-                                ),
-                              );
-                            }).toList(),
+                                      const SizedBox(width: 10),
+                                    ],
+                                  )
+                                  : Row(
+                                    children: [
+                                      Text(
+                                          selectedWebsite,
+                                      style: dropDownTextStyle
+                                      ),
+                                      const SizedBox(width: 10),
+                                    ],
+                                  ),
+                              value: selectedWebsite,
+                              onChanged: (newValue) {
+                                setState(() {
+                                  isSelectedFromDropDwn = true;
+                                  selectedWebsite = newValue;
+                                });
+                              },
+                              items: [
+                                'Codelattice',
+                                'Alddinpro - GA4',
+                              ].map((String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value,
+                                      style: dropDownTextStyle
+                                  ),
+                                );
+                              }).toList(),
+                            ),
                           ),
                         ),
                       ),
 
                       const SizedBox(width: 0),
 
-                      Container(
-                        height: 43,
-                        padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
-                        decoration: BoxDecoration(
-                          color: whiteColor,
-                          borderRadius: BorderRadius.circular(5),
+                      Card(
+                        elevation: 2,
+                        child: Container(
+                          height: 43,
+                          padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                          decoration: BoxDecoration(
+                            color: whiteColor,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: const Icon(
+                            Icons.calendar_month,
+                            color: blackColor,
+                            size: 20,
+                          )
                         ),
-                        child: const Icon(
-                          Icons.calendar_month,
-                          color: titleTextColor,
-                          size: 20,
-                        )
                       ),
 
                       const SizedBox(width: 0),
 
                       Padding(
                         padding: const EdgeInsets.only(right: 10),
-                        child: Container(
-                          height: 43,
-                          padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
-                          decoration: BoxDecoration(
-                            color: whiteColor,
-                            borderRadius: BorderRadius.circular(5),
+                        child: Card(
+                          elevation: 2,
+                          child: Container(
+                            height: 43,
+                            padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                            decoration: BoxDecoration(
+                              color: whiteColor,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: const Icon(
+                              Icons.download,
+                              color: blackColor,
+                              size: 20,
+                            )
                           ),
-                          child: const Icon(
-                            Icons.download,
-                            color: titleTextColor,
-                            size: 20,
-                          )
                         ),
                       ),
                     ],
