07f50e363185cfbdfd3b89a72b5ce42bb8aa597b
Ashna <ashnakanakaraj704@gmail.com>
2024-04-22 10:41:56 +0400

    fixed the issue in Visitors screen,added visitors trending time graph

--- lib/features/tabs/visitors_screen.dart
+++ lib/features/tabs/visitors_screen.dart
@@ -1,21 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-
+import 'package:reddog_mobile_app/features/auth/login_screen.dart';
+import 'package:reddog_mobile_app/features/common/notification_list_screen.dart';
+import 'package:reddog_mobile_app/models/user_by_gender_model.dart';
+import 'package:reddog_mobile_app/models/visitor_info_tile_model.dart';
+import 'package:reddog_mobile_app/providers/registered_website_provider.dart';
+import 'package:reddog_mobile_app/providers/user_profile_provider.dart';
+import 'package:reddog_mobile_app/providers/visitor_provider.dart';
+import 'package:reddog_mobile_app/repositories/common_repository.dart';
+import 'package:reddog_mobile_app/repositories/user_repository.dart';
+import 'package:reddog_mobile_app/repositories/visitor_repository.dart';
+import 'package:reddog_mobile_app/styles/colors.dart';
+import 'package:reddog_mobile_app/widgets/tiles.dart';
+import 'package:reddog_mobile_app/widgets/tiles_full_width.dart';
 import '../../core/ui_state.dart';
-import '../../providers/registered_website_provider.dart';
-import '../../providers/user_profile_provider.dart';
-import '../../providers/visitor_provider.dart';
-import '../../repositories/common_repository.dart';
-import '../../repositories/user_repository.dart';
-import '../../repositories/visitor_repository.dart';
-import '../../styles/colors.dart';
+import '../../models/user_by_age_group_model.dart';
+import '../../models/visitor_trending_time_model.dart';
 import '../../styles/text_styles.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:countries_world_map/countries_world_map.dart';
+import 'package:countries_world_map/data/maps/world_map.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'dart:math' as math;
 import '../../utilities/shared_prefernces.dart';
-import '../../widgets/tiles.dart';
-import '../../widgets/tiles_full_width.dart';
-import '../auth/login_screen.dart';
-import '../common/notification_list_screen.dart';
+import 'package:reddog_mobile_app/models/user_by_newturned_model.dart';
 
 class VisitorsScreen extends StatefulWidget {
   const VisitorsScreen({super.key});
@@ -40,11 +53,25 @@ class _VisitorsScreenState extends State<VisitorsScreen> {
     );
   }
 
+  getUserByTrendingTimeMethod() async{
+    await visitorProvider.getUserByTrendingTimeList(
+        _selectedFromDate != null ?
+        '${DateFormat('yyyy-MM-dd').format(_selectedFromDate)}' : formattedInitialdDate,
+        _selectedToDate != null ?  '${DateFormat('yyyy-MM-dd').format(_selectedToDate)}' : formattedDate
+    );
+  }
+
   String storedWebsiteId = '';
 
   void getStoredWebsiteId() async{
     storedWebsiteId = await getValue('websiteId');
   }
+
+  // visitors trending time graph color declarations
+  List<Color> gradientColors = [
+    Colors.cyan,
+    Colors.blue,
+  ];
 
   @override
   void initState(){
@@ -54,6 +81,7 @@ class _VisitorsScreenState extends State<VisitorsScreen> {
     userProfileProvider.getProfile();
     registeredWebsiteProvider.getRegisteredWebsiteList();
     getVisitorTileMethod();
+    getUserByTrendingTimeMethod();
   }
 
   // drop down menu variables
@@ -88,6 +116,8 @@ class _VisitorsScreenState extends State<VisitorsScreen> {
       setState(() {
         _selectedFromDate = picked.start;
         _selectedToDate = picked.end;
+        getVisitorTileMethod();
+        getUserByTrendingTimeMethod();
       });
     }
   }
@@ -417,6 +447,7 @@ class _VisitorsScreenState extends State<VisitorsScreen> {
                               selectedWebsite = val;
                               setValue('websiteId', val);
                               getVisitorTileMethod();
+                              // getUserByTrendingTimeMethod();
                             });
                           })
 
@@ -446,6 +477,7 @@ class _VisitorsScreenState extends State<VisitorsScreen> {
         return visitorProvider;
       },
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
 
         // Visitor Tile Data
@@ -505,9 +537,298 @@ class _VisitorsScreenState extends State<VisitorsScreen> {
               return Container();
             }
           }),
+
+          const SizedBox(height: 15),
+        // Visitor trending time graph
+          Text(
+            'Visitors trending time?',
+            style: normalTextStyle,
+          ),
+          const SizedBox(height: 10),
+          Card(
+            elevation: 2,
+            shadowColor: whiteColor,
+            child: Container(
+                padding: const EdgeInsets.all(10),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(3),
+                  color: whiteColor,
+                ),
+                child:
+                Stack(
+                  children: <Widget>[
+                    AspectRatio(
+                      aspectRatio: 1.70,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          right: 18,
+                          left: 12,
+                          top: 10,
+                          bottom: 12,
+                        ),
+                        child: trendingTimeGraphWidget(),
+                        // LineChart(
+                        //   mainData(),
+                        // ),
+                      ),
+                    ),
+                  ],
+                )
+            ),
+          ),
         ],
       ),
     );
   }
 
+  Widget trendingTimeGraphWidget(){
+    return ChangeNotifierProvider<VisitorProvider>(
+      create: (ctx) {
+        return visitorProvider;
+      },
+      child: Consumer<VisitorProvider>(
+        builder: (ctx, data, _) {
+          var state = data.userByVisitorsTrendingTimeLiveData().getValue();
+          print(state);
+          if (state is IsLoading) {
+            return SizedBox();
+          }else if (state is Success) {
+            return SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: SizedBox(
+                width: 500,
+                child: LineChart(
+                    mainData(
+                        data.userByVisitorsTrendingTimeModel.data ?? []
+                      // data.userByVisitorsTrendingTimeModel.data!.length,
+                      // data.userByVisitorsTrendingTimeModel.data!.length,
+                    )
+                ),
+              ),
+            );
+          }else if (state is Failure) {
+            return Text('Failed to load!!');
+          }else {
+            return SizedBox();
+          }
+        },
+      ),
+    );
+  }
+
+  LineChartData mainData(List<TrendingTimeData> data) {
+    List<FlSpot> spots = [];
+    double maxYValue = 0;
+    // Determine the length of your date data list
+    int dataLength = data.length;
+
+
+    // Create FlSpot instances from your data
+    for (int i = 0; i < data.length; i++) {
+      double yValue = double.parse(data[i].value ?? '0');
+      spots.add(FlSpot(i.toDouble(), double.parse(data[i].value ?? '0')));
+
+      // Update maxYValue if current Y-value is greater
+      if (yValue > maxYValue) {
+        maxYValue = yValue;
+      }
+    }
+
+    // Adjust maxYValue if needed (e.g., add some buffer)
+    maxYValue += 2; // Adding buffer for better visualization
+
+    return LineChartData(
+      gridData: FlGridData(
+        show: true,
+        drawVerticalLine: false,
+        drawHorizontalLine: true,
+        horizontalInterval: 3,
+        verticalInterval: 3,
+        getDrawingHorizontalLine: (value) {
+          return const FlLine(
+            color: dividerColor,
+            strokeWidth: 1,
+          );
+        },
+        getDrawingVerticalLine: (value) {
+          return const FlLine(
+            color: Colors.black,
+            strokeWidth: 1,
+          );
+        },
+      ),
+      titlesData: FlTitlesData(
+        show: true,
+        rightTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false,),
+        ),
+        topTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            reservedSize: 35,
+            interval: 1,
+            getTitlesWidget: bottomTitleWidgets,
+          ),
+        ),
+        leftTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            interval: 3,
+            getTitlesWidget: leftTitleWidgets,
+            reservedSize: 42,
+          ),
+        ),
+      ),
+      borderData: FlBorderData(
+        show: false,
+        border: Border.all(color: const Color(0xff37434d)),
+      ),
+      minX: 0,
+      maxX: dataLength.toDouble(),
+      minY: 0,
+      maxY: maxYValue,
+      lineBarsData: [
+        LineChartBarData(
+          spots: spots,
+          // spots: [
+          //   FlSpot(1, 3),
+          //   FlSpot(2, 5),
+          //   FlSpot(3, 2),
+          //   FlSpot(4, 10),
+          //   FlSpot(5, 4),
+          //   FlSpot(6, 4),
+          //   FlSpot(7, 3),
+          //   FlSpot(8, 4),
+          // ],
+          isCurved: true,
+          gradient: LinearGradient(
+            colors: gradientColors,
+          ),
+          barWidth: 2,
+          isStrokeCapRound: true,
+          dotData: const FlDotData(
+            show: true,
+          ),
+          belowBarData: BarAreaData(
+            show: true,
+            gradient: LinearGradient(
+              colors: gradientColors
+                  .map((color) => color.withOpacity(0.3))
+                  .toList(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    return ChangeNotifierProvider<VisitorProvider>(
+      create: (ctx) {
+        return visitorProvider;
+      },
+      child: Consumer<VisitorProvider>(
+        builder: (ctx, data, _) {
+          var state = data.userByVisitorsTrendingTimeLiveData().getValue();
+          print(state);
+          if (state is IsLoading) {
+            return SizedBox();
+          }else if (state is Success) {
+            const style = TextStyle(
+                fontSize: 12,
+                fontFamily: 'Barlow-Regular',
+                color: titleTextColor
+            );
+
+            // Find the corresponding date from dataList based on value
+            final int index = value.toInt();
+            if (index < 0 || index >= data.userByVisitorsTrendingTimeModel.data!.length) {
+              return Container(); // Return an empty container if index is out of bounds
+            }
+
+            final String date = data.userByVisitorsTrendingTimeModel.data![index].date ?? ''; // Get date from dataList
+
+            final text = Text(date, style: style);
+
+            return Padding(
+              padding: const EdgeInsets.only(right: 25,top: 5),
+              child: SideTitleWidget(
+                axisSide: meta.axisSide,
+                child: text,
+                angle: -math.pi / 3.5,
+              ),
+            );
+          }else if (state is Failure) {
+            return Text('Failed to load!!');
+          }else {
+            return SizedBox();
+          }
+        },
+      ),
+    );
+  }
+
+  Widget leftTitleWidgets(double value, TitleMeta meta) {
+    return ChangeNotifierProvider<VisitorProvider>(
+      create: (ctx) {
+        return visitorProvider;
+      },
+      child: Consumer<VisitorProvider>(
+        builder: (ctx, data, _) {
+          var state = data.userByVisitorsTrendingTimeLiveData().getValue();
+          print(state);
+          if (state is IsLoading) {
+            return SizedBox();
+          }else if (state is Success) {
+            const style = TextStyle(
+                fontSize: 12,
+                fontFamily: 'Barlow-Regular',
+                color: titleTextColor
+            );
+
+            // Find the corresponding date from dataList based on value
+            int startValue = 0;
+            int interval = 1;
+            int index = value.toInt();
+            // Adjust the maximum value
+            int maxDisplayedValue = 13;
+            if (index < 0 || index >= maxDisplayedValue) {
+              return Container(); // Return an empty container if index is out of bounds
+            }
+
+            // if (index < 0 || index >= data.userByVisitorsTrendingTimeModel.data!.length) {
+            //   return Container(); // Return an empty container if index is out of bounds
+            // }
+
+            // final text = (startValue + index * interval).toString();
+            final String date = (startValue + index * interval).toString();
+            //
+            final text = Text(
+                date,
+                softWrap: false,
+                maxLines: 1,
+                style: style
+            );
+
+            return Padding(
+              padding: const EdgeInsets.only(right: 20,top: 5),
+              child: SideTitleWidget(
+                axisSide: meta.axisSide,
+                child: text,
+                angle: 0,
+              ),
+            );
+          }else if (state is Failure) {
+            return Text('Failed to load!!');
+          }else {
+            return SizedBox();
+          }
+        },
+      ),
+    );
+  }
 }
